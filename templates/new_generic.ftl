jugmilano/jugmilano.github.io

commit 1fb46722496731508186e2baeef8a7eac43c2c5d

Update new_generic.ftl: add language info

templates/new_generic.ftl
@@ -44,13 +44,17 @@ L'incontro si terrà ${content.meetingdate?string.@jugmilanomeetingdate}
 
 <!-- here was formspree -->
 
-<#if content.miscdetails??>
 <p>
-<div id="miscdetails" style="text-align: justify">
-${content.miscdetails}
+<div>Il talk sarà in ${content.language}.</div>
+</p>
+
+<p>
+<div id="abstract">
+<span style="font-weight:bold">Abstract dell'intervento: </span>
+<br/>
+${content.abstract!""}
 </div>
 </p>
-</#if>
 
 <p>
 <div id="abstract">

commit 79644c73c8616307aa733deef936c153f750a5cd

re-introduce miscdetails in new_generic FTL template
regressed from 1fb46722496731508186e2baeef8a7eac43c2c5d

Signed-off-by: tarilabs <matteo.mortari@gmail.com>

templates/new_generic.ftl
@@ -48,6 +48,14 @@ L'incontro si terrà ${content.meetingdate?string.@jugmilanomeetingdate}
 <div>Il talk sarà in ${content.language}.</div>
 </p>
 
+<#if content.miscdetails??>
+<p>
+<div id="miscdetails" style="text-align: justify">
+${content.miscdetails}
+</div>
+</p>
+</#if>
+
 <p>
 <div id="abstract">
 <span style="font-weight:bold">Abstract dell'intervento: </span>